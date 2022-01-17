aa9124ffbab5221a50d2afa15797da460cf5d18e
安洋 <496812133@qq.com>
2022-01-17 16:55:48 +0800

    user login

--- src/main/java/com/dingjiangying/webmonitor/controller/UserController.java
+++ src/main/java/com/dingjiangying/webmonitor/controller/UserController.java
@@ -2,16 +2,21 @@ package com.dingjiangying.webmonitor.controller;
 
 import com.dingjiangying.webmonitor.dao.UserPoMapper;
 import com.dingjiangying.webmonitor.po.UserPo;
+import com.dingjiangying.webmonitor.po.UserPoExample;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @Controller
 @RequestMapping
@@ -22,19 +27,49 @@ public class UserController {
     @Autowired
     CommonController commonController;
 
-//    @RequestMapping("login")
-//    public String getLogin(Model model){
-//        commonController.putUserName(model);
-//        return "/login";
-//    }
-//
-//    @RequestMapping("login?{param}")
-//    public String getLogin(@PathVariable("param") String param, Model model){
-//        commonController.putUserName(model);
-//        model.addAttribute("param", param);
-//        return "/login";
-//    }
-//
+    @Resource
+    UserPoMapper userPoMapper;
+
+    @GetMapping(value = "login")
+    public String getLogin(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUserName = authentication.getName();
+            model.addAttribute("currentUser", currentUserName);
+        } else {
+            model.addAttribute("currentUser", "游客");
+        }
+        return "/login";
+    }
+
+
+    @PostMapping(value = "login")
+    public String postLogin(Model model, String username, String password) throws Exception {
+        UserPoExample example = new UserPoExample();
+        example.createCriteria().andUserNameEqualTo(username);
+        List<UserPo> users = userPoMapper.selectByExample(example);
+        if (users == null || users.size() == 0) {
+            //未找到用户名
+            model.addAttribute("error", "未找到用户名");
+            return "/login";
+        }
+//        默认同名用户只有一个
+        UserPo user = users.get(0);
+        if (user.getUserPassword() == null) {
+            model.addAttribute("error", "用户数据异常");
+            return "/login";
+        }
+
+        if (user.getUserPassword().equals(password)) {
+            model.addAttribute("success", "登陆成功");
+            return "/task/list";
+        } else {
+            model.addAttribute("error", "用户密码错误");
+            return "/login";
+        }
+
+    }
+
 //    @RequestMapping("/info")
 //    public String getGreenPage(){
 //        return "alert";
--- src/main/java/com/dingjiangying/webmonitor/service/UserService.java
+++ src/main/java/com/dingjiangying/webmonitor/service/UserService.java
@@ -1,11 +1,25 @@
 package com.dingjiangying.webmonitor.service;
 
+import com.dingjiangying.webmonitor.dao.UserPoMapper;
 import com.dingjiangying.webmonitor.po.UserPo;
+import com.dingjiangying.webmonitor.po.UserPoExample;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class UserService {
+    @Resource
+    UserPoMapper userPoMapper;
+
     public UserPo selectUser(String username) {
         return new UserPo();
     }
+
+//    public boolean checkLogin(String username, String pwd) {
+//
+//    }
+
 }
