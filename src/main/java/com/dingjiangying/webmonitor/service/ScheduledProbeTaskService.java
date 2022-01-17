2bc494d4431f55b81097dd663775fb9b8e686d7f
dingjiangying <dingjiangying@meituan.com>
2022-01-17 23:31:27 +0800

    每分钟拉取一次任务1

--- src/main/java/com/dingjiangying/webmonitor/service/ScheduledProbeTaskService.java
+++ src/main/java/com/dingjiangying/webmonitor/service/ScheduledProbeTaskService.java
@@ -1,5 +1,21 @@
 package com.dingjiangying.webmonitor.service;
 
+import com.dingjiangying.webmonitor.dao.ProbePoMapper;
+import com.dingjiangying.webmonitor.dao.TaskPoMapper;
+import com.dingjiangying.webmonitor.po.ProbePo;
+import com.dingjiangying.webmonitor.po.TaskPo;
+import com.dingjiangying.webmonitor.util.Util;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSON;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -14,7 +30,7 @@ public class ScheduledProbeTaskService {
     @Autowired
     JavaMailSenderImpl javaMailSender;
 
-    @Scheduled(fixedRate = 20000)//20s一次
+//    @Scheduled(fixedRate = 20000)//20s一次
     public void sendmail() {
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setSubject("邮件发送测试");
@@ -22,6 +38,119 @@ public class ScheduledProbeTaskService {
         simpleMailMessage.setFrom("1104349906@qq.com");
         simpleMailMessage.setTo("2000022759@stu.pku.edu.cn");
         javaMailSender.send(simpleMailMessage);
+    }
+
+    @Autowired
+    ProbePoMapper probePoMapper;
+
+    @Autowired
+    TaskPoMapper taskPoMapper;
+
+    @Scheduled(fixedRate = 60000)//一分钟拉一次
+    public void pullTasks(Integer probeId) {
+        String activeTaskList = probePoMapper.selectByPrimaryKey(probeId).getActiveTaskList();
+        List<String> activeTaskIdList = Util.getList(activeTaskList);
+        List<Integer> users2 = JSON.parseArray(activeTaskList, Integer.class);
+        List<String> taskIdList = new ArrayList<>();
+//        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+//        simpleMailMessage.setSubject("邮件发送测试");
+//        simpleMailMessage.setText("测试下");
+//        simpleMailMessage.setFrom("1104349906@qq.com");
+//        simpleMailMessage.setTo("2000022759@stu.pku.edu.cn");
+//        javaMailSender.send(simpleMailMessage);
+        String localTaskPath = "./localTasks.txt";
+        File localTasks = new File(localTaskPath);
+        try {
+            // 检查目标文件是否存在，不存在则创建
+            if (!localTasks.exists()) {
+                localTasks.createNewFile();// 创建目标文件
+            }
+            //所有taskPo的json列表
+            List<String> localTaskStringList = getFileContext(localTaskPath);
+
+            //只留下update里没有的task
+            List<TaskPo> localTaskList = localTaskStringList.stream()
+                    .map(taskString -> JSONObject.parseObject(taskString, TaskPo.class))
+                    .filter(task -> !activeTaskIdList.contains(String.valueOf(task.getTaskId())))
+                    .collect(Collectors.toList());
+
+            //这里最好在mapper写个按id列表查询
+            for (String activeId : activeTaskIdList) {
+                TaskPo taskPo = taskPoMapper.selectByPrimaryKey(Integer.parseInt(activeId));
+                localTaskList.add(taskPo);
+            }
+
+            //更新probeTask列表
+            List<>
+
+            //写回文件
+            localTaskStringList = localTaskList.stream().map(JSONObject::toJSONString).collect(Collectors.toList());
+            writeFileContext(localTaskStringList, localTaskPath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //从文件按行读入列表
+    public static List<String> getFileContext(String path) throws Exception {
+        FileReader fileReader =new FileReader(path);
+        BufferedReader bufferedReader =new BufferedReader(fileReader);
+        List<String> list =new ArrayList<>();
+        String str=null;
+        while((str=bufferedReader.readLine())!=null) {
+            if(str.trim().length()>2) {
+                list.add(str);
+            }
+        }
+        return list;
+    }
+
+    //将列表按行写入文件
+    public static void writeFileContext(List<String>  strings, String path) throws Exception {
+        File file = new File(path);
+        //如果没有文件就创建
+        if (!file.isFile()) {
+            file.createNewFile();
+        }
+        BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+        if(System.getProperty("os.name").equals("Windows")){
+            for (String l:strings){
+                writer.write(l + "\r\n");
+            }
+        }else{
+            for (String l:strings){
+                writer.write(l + "\n");
+            }
+        }
+
+        writer.close();
+    }
+
+    public void createFile() throws IOException {
+        String filePath = "D:/a/b";
+        File dir = new File(filePath);
+        // 一、检查放置文件的文件夹路径是否存在，不存在则创建
+        if (!dir.exists()) {
+            dir.mkdirs();// mkdirs创建多级目录
+        }
+        File checkFile = new File(filePath + "/filename.txt");
+        FileWriter writer = null;
+        try {
+            // 二、检查目标文件是否存在，不存在则创建
+            if (!checkFile.exists()) {
+                checkFile.createNewFile();// 创建目标文件
+            }
+            // 三、向目标文件中写入内容
+            // FileWriter(File file, boolean append)，append为true时为追加模式，false或缺省则为覆盖模式
+            writer = new FileWriter(checkFile, true);
+            writer.append("your content");
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != writer)
+                writer.close();
+        }
     }
 
 }
--- src/main/java/com/dingjiangying/webmonitor/util/Util.java
+++ src/main/java/com/dingjiangying/webmonitor/util/Util.java
@@ -1,6 +1,10 @@
 package com.dingjiangying.webmonitor.util;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import javax.servlet.http.HttpSession;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 
 public class Util {
     public static String getCurrentUserName(HttpSession session){
@@ -8,5 +12,10 @@ public class Util {
             return "游客";
         }
         return (String) session.getAttribute("currentUser");
+    }
+
+    public static List<String> getList(String list){//"[1,2,3]"
+        String[] strignList = list.replace("[]","").split(",");
+        return Arrays.asList(strignList);
     }
 }
