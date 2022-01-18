505fb1c732316070b8865709941aa15faadfd144
安洋 <496812133@qq.com>
2022-01-19 01:33:33 +0800

    dash board

--- src/main/java/com/dingjiangying/webmonitor/controller/DashBoardController.java
+++ src/main/java/com/dingjiangying/webmonitor/controller/DashBoardController.java
@@ -1,8 +1,12 @@
 package com.dingjiangying.webmonitor.controller;
 
+import com.dingjiangying.webmonitor.util.Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping(value = "/dashboard")
@@ -12,7 +16,8 @@ public class DashBoardController {
     CommonController commonController;
 
     @RequestMapping("/summury")
-    public String getGreenPage(){
+    public String getGreenPage(Model model, HttpSession session){
+        model.addAttribute("currentUser", Util.getCurrentUserName(session));
         return "monitor";
     }
 
